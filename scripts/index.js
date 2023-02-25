94b84dec14b4d9418c865d9730f1b6e06bb12728
Vasiliy Manuilov <v.manyilov@yandex.ru>
2023-02-25 21:49:44 +0300

    feat: попап добавить изображение

--- scripts/index.js
+++ scripts/index.js
@@ -1,80 +1,68 @@
 //переменные для попап профиля
-const editPopup = document.querySelector('#popup-profile'); //находим модальное окно для открытия
-const editClosePopup = document.querySelector('#popup-profile'); //находим модальное окно для закрытия
-const openPopupButton = document.querySelector('.profile__edit-button'); //находим кнопку редактировать профиль
-const closePopupButton = document.querySelector('#btn-close-editProfile'); //находим кнопку закрыть попап профиля
-let userName = document.querySelector('.profile__title'); //находим имя в профиле
-let jobName = document.querySelector('.profile__text'); //находим занятость в профиле
-let formElement = document.querySelector('.popup__form'); // Находим форму
-let nameInput = document.querySelector('.popup__input_type_name'); // Находим поле имя в форме
-let jobInput = document.querySelector('.popup__input_type_job'); // Находим поле занятость в форме
+const editPopup = document.querySelector(".popup_profile"); //находим модальное окно для открытия
+const editClosePopup = document.querySelector(".popup_profile"); //находим модальное окно для закрытия
+const openPopupButton = document.querySelector(".profile__edit-button"); //находим кнопку редактировать профиль
+const closePopupButton = document.querySelector(".popup__button-close"); //находим кнопку закрыть попап профиля
+let userName = document.querySelector(".profile__title"); //находим имя в профиле
+let jobName = document.querySelector(".profile__text"); //находим занятость в профиле
+let formElement = document.querySelector(".popup__form"); // Находим форму
+let nameInput = document.querySelector(".popup__input_type_name"); // Находим поле имя в форме
+let jobInput = document.querySelector(".popup__input_type_job"); // Находим поле занятость в форме
 //переменные для попап добавить изображение
-const openAddImagePopupButton = document.querySelector('.profile__add-button'); //находим кнопку добавить изображение
-const addImagePopup = document.querySelector('#popup-image');//доступ к добавлению видимости окна добавить изображение
-const сloseaddImagePopup = document.querySelector('#popup-image'); //доступ к удалению класса видимости попап добавить изображение
-const closePopupButtonImage = document.querySelector('#btn-close-addImage'); //кнопка закрыть попап добавить изображение
-let formImage = document.querySelector('.popup__form-image'); // Находим форму image
-let headingInput = document.querySelector('.popup__input_type_heading'); // Находим поле название изображения в форме
-let imageInput = document.querySelector('.popup__input_type_url'); // Находим поле ссылка на картинку в форме
-let elements = document.querySelector('.elements'); //получаем доступ для добавления пункта в список
+const openAddImagePopupButton = document.querySelector(".profile__add-button"); //находим кнопку добавить изображение
+const addImagePopup = document.querySelector(".popup_image"); //доступ к добавлению видимости окна добавить изображение
+const сloseaddImagePopup = document.querySelector(".popup_image"); //доступ к удалению класса видимости попап добавить изображение
+const closePopupButtonImage = document.querySelector(
+  ".popup__button-close_theme_addImage"
+); //кнопка закрыть попап добавить изображение
+let formImage = document.querySelector(".popup__form-image"); // Находим форму image
+let headingInput = document.querySelector(".popup__input_type_heading"); // Находим поле название изображения в форме
+let imageInput = document.querySelector(".popup__input_type_url"); // Находим поле ссылка на картинку в форме
+let elements = document.querySelector(".elements"); //получаем доступ для добавления пункта в список
 
 const initialCards = [
   {
-    name: 'Архыз',
-    alt: 'Тающий снег на вершине Архыза',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: "Архыз",
+    alt: "Тающий снег на вершине Архыза",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: 'Челябинская область',
-    alt: 'Изгиб русла реки',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    alt: "Изгиб русла реки",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    name: 'Иваново',
-    alt: 'Человейники',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    alt: "Человейники",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    name: 'Камчатка',
-    alt: 'равнина перед сопкой',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    alt: "равнина перед сопкой",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    name: 'Холмогорский район',
-    alt: 'Железная дорога через лес',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    alt: "Железная дорога через лес",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    name: 'Байкал',
-    alt: 'Заледенелое побережье Байкала',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
+    name: "Байкал",
+    alt: "Заледенелое побережье Байкала",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
 
-// Ниже Функции для попап профиля
-
-//добавляем класс для видимости попап профиля
-function openPopup() {
-  editPopup.classList.add('popup_opened');
+// открыть попап
+function openPopup(addElement) {
+  addElement.classList.add("popup_opened");
   nameInput.value = userName.textContent; //Получаем значение заголовка из профиля
   jobInput.value = jobName.textContent; //Получаем значения в занятость профиля
 }
 
-//удаляем класс для скрытия модального окна
-function closePopup() {
-  editClosePopup.classList.remove('popup_opened');
-}
-
-//ниже функции для попап добавления изображений
-
-//добавляем класс для видимости попап добавить изображение
-function openAddImagePopup() {
-  addImagePopup.classList.add('popup_opened');
-}
-
-//удаляем класс для скрытия попап добавить изображение
-function closeAddImagePopup() {
-  сloseaddImagePopup.classList.remove('popup_opened');
+// скрыть popup
+function closePopup(delElement) {
+  delElement.classList.remove("popup_opened");
 }
 
 //отправляем форму попап профиля
@@ -82,57 +70,58 @@ function handleFormSubmit(evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value; //Получаем значение поля имени и вставляем измененные значения в заголовок профиля
   jobName.textContent = jobInput.value; //Получаем значение поля имени и вставляем измененные значения в занятость профиля
-  closePopup();
+  closePopup(editClosePopup);
 }
 
-//добавляем пункт карточки в список
-function createItem(item) {
-  const newItem = document.querySelector('#elements__item-Template').content.cloneNode(true); //получаем содержимое шаблона и клонируем
-  const itemImage = newItem.querySelector('.elements__image'); //получаем доступ к изображению в шаблоне
-  itemImage.setAttribute('src', item.link); //дабавить атрибут ссылка на изображение из массива
-  itemImage.setAttribute('alt', item.alt); //добавить алт для изображения из массива
-  const itemTitle = newItem.querySelector('.elements__title'); //получаем доступ к загловку в шаблоне
-  itemTitle.textContent = item.name; //копировать заголовок из массива
-  const buttonLike = newItem.querySelector('.elements__button-like'); //доступ к кнопке лайк
-  // оброботчик события кнопки лайк
-  buttonLike.addEventListener('click', function (evt) {
-  evt.target.classList.toggle('elements__button-like_active');
-  });
-  const buttonDelete = newItem.querySelector('.elements__btn-delete'); //доступ к кнопке удалить пункт списка
-  buttonDelete.addEventListener('click', buttonDeleteItem); //обработчки удаления пукта в списке
+//добавить изображение в список по шаблону
+function createCard(card) {
+  const newCard = document.querySelector("#elements__item-Template").content.cloneNode(true); //получаем содержимое шаблона и клонируем
 
-  return newItem;
+  newCard.querySelector(".elements__image").src = card.link; //ссылка на изображение
+  newCard.querySelector(".elements__image").alt = card.alt;//алт для изображения
+  newCard.querySelector(".elements__title").textContent = card.name; //заголовок в шаблоне
+
+  newCard.querySelector(".elements__btn-delete").addEventListener('click', buttonDeleteCard); //удаления изображения в списке
+  newCard.querySelector(".elements__button-like").addEventListener('click', buttonLike); //кнопка лайк
+
+  return newCard;
 }
 
-initialCards.forEach(item => {
-  elements.append(createItem(item));
+//перебираем массив, выполняем код для шаблона и добавляем в список
+initialCards.forEach((card) => {
+  elements.append(createCard(card));
 });
- //перебираем массив, выполняем код для шаблона и добавляем в список
-//elements.append(newItem); //вставить код в html список
 
-//отправляем форму добавить изображение
+//отправить форму добавить изображение
 function imageFormSubmit(evt) {
   evt.preventDefault();
-  const formImage = evt.target;
-  const newItem = createItem({
+  const newCard = createCard({
     name: headingInput.value,
-    link: imageInput.value
+    link: imageInput.value,
   });
-  elements.prepend(newItem);
-  closeAddImagePopup();
+  elements.prepend(newCard);
+  headingInput.value = "";
+  imageInput.value = "";
+  closePopup(сloseaddImagePopup);
 }
 
-// удаление пункта в списке
-function buttonDeleteItem(evt) {
-  const knopkaDelete = evt.target;
-  const item = knopkaDelete.closest('.elements__item');
-  item.remove();
+// удалить изображения в списке
+function buttonDeleteCard(evt) {
+  evt.target.closest(".elements__item").remove();
 }
 
-openPopupButton.addEventListener('click', openPopup); //открываем попап профиля
-closePopupButton.addEventListener('click', closePopup); //закрываем попап профиля
-openAddImagePopupButton.addEventListener('click', openAddImagePopup); //открываем попап добавить изображение
-closePopupButtonImage.addEventListener('click', closeAddImagePopup); //закрываем попап добавить изображение
-formElement.addEventListener('submit', handleFormSubmit); // Обработчик «отправки» формы
-formImage.addEventListener('submit', imageFormSubmit); // Обработчик «отправки» формы add-image
+ // оброботчик события кнопки лайк
+function buttonLike(evt) {
+  evt.target.classList.toggle("elements__button-like_active");
+};
 
+openPopupButton.addEventListener("click", () => openPopup(editPopup)); //открываем попап профиля
+closePopupButton.addEventListener("click", () => closePopup(editClosePopup)); //закрываем попап профиля
+openAddImagePopupButton.addEventListener("click", () =>
+  openPopup(addImagePopup)
+); //открываем попап добавить изображение
+closePopupButtonImage.addEventListener("click", () =>
+  closePopup(сloseaddImagePopup)
+); //закрываем попап добавить изображение
+formElement.addEventListener("submit", handleFormSubmit); // Обработчик «отправки» формы
+formImage.addEventListener("submit", imageFormSubmit); // Обработчик «отправки» формы add-image
