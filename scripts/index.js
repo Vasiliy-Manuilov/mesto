f26a053e0786e3057411290a3c7037e29763691a
Vasiliy Manuilov <v.manyilov@yandex.ru>
2023-03-07 18:57:43 +0300

    feat: закрытие Esc и оверлей

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
+const popups = Array.from(document.querySelectorAll('.popup'));
+
 /** Редактирование профиля */
 const profilePopup = document.querySelector('.popup_type_profile');
+const buttonCloseProfile = profilePopup.querySelector('.popup__button-close');
 const popupEditProfile = document.querySelector('.profile__edit-button');
-const buttonCloseProfile = document.querySelector('.popup__button-close');
 const userName = document.querySelector('.profile__title');
 const jobName = document.querySelector('.profile__text');
 const formProfile = document.querySelector('.popup__form');
@@ -11,9 +13,7 @@ const jobUserProfile = document.querySelector('.popup__input_type_job');
 /** Добавить карточку */
 const popupAddCard = document.querySelector('.popup_type_image');
 const buttonAddCard = document.querySelector('.profile__add-button');
-const buttonCloseCard = document.querySelector(
-  '.popup__button-close_type_image'
-);
+const buttonCloseCard = popupAddCard.querySelector('.popup__button-close');
 const formImage = document.querySelector('.popup__form-image');
 const headingCard = document.querySelector('.popup__input_type_heading');
 const urlCard = document.querySelector('.popup__input_type_url');
@@ -21,20 +21,28 @@ const cardAddHtml = document.querySelector('.elements');
 
 /** Увеличить карточку */
 const popupCardViewer = document.querySelector('.popup_type_viewer');
-const buttonCloseViewer = document.querySelector(
-  '.popup__button-close_type_viewer'
-);
+const buttonCloseViewer = popupCardViewer.querySelector('.popup__button-close');
 const imageViewer = document.querySelector('.popup__image');
 const headingViewer = document.querySelector('.popup__heading-image');
 
 /** Открыть попап */
-function openPopup(addElement) {
-  addElement.classList.add('popup_opened');
+function openPopup(elementPopup) {
+  elementPopup.classList.add('popup_opened');
+  document.addEventListener('keydown', closePopupEsc);
+}
+
+/** Закрыть кнопкой Esc*/
+function closePopupEsc(evt) {
+  if (evt.key === 'Escape') {
+    const popupEscape = document.querySelector('.popup_opened');
+    closePopup(popupEscape);
+  }
 }
 
 /** Закрыть попап*/
-function closePopup(delElement) {
-  delElement.classList.remove('popup_opened');
+function closePopup(elementPopup) {
+  elementPopup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closePopupEsc);
 }
 
 /** Отправить форму попап профиля*/
@@ -108,10 +116,19 @@ popupEditProfile.addEventListener('click', () => {
   openPopup(profilePopup);
 });
 
-buttonCloseProfile.addEventListener('click', () => closePopup(profilePopup));
+/** Закрыть по кнопке и кликом по оверлей*/
+popups.forEach((element) => {
+  element.addEventListener('mousedown', function (evt) {
+    if (evt.target.classList.contains('popup_opened')) {
+      closePopup(element);
+    }
+    if (evt.target.classList.contains('popup__button-close')) {
+      closePopup(element);
+    }
+  });
+});
+
 buttonAddCard.addEventListener('click', () => openPopup(popupAddCard));
-buttonCloseCard.addEventListener('click', () => closePopup(popupAddCard));
-buttonCloseViewer.addEventListener('click', () => closePopup(popupCardViewer));
 
 formProfile.addEventListener('submit', handleSubmitProfileForm);
 formImage.addEventListener('submit', handleSubmitImageForm);
